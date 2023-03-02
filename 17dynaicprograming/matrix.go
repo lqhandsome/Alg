f8611fadda4381694566b6ccf03aa82fd218a377
lqhandsome <lqhandsome@163.com>
2023-03-02 23:29:58 +0800

    矩阵问题，动态规划方式解决

--- 17dynaicprograming/matrix.go
+++ 17dynaicprograming/matrix.go
@@ -49,6 +49,38 @@ func (p Matrix) Find(dist int, x, y int) {
 	}
 }
 
+// 动态规划计算最短路径
+func (p Matrix) Dyna() (minDist int) {
+	var statArr [][]int
+	statArr = make([][]int, p.End+1)
+	for k, _ := range statArr {
+		statArr[k] = make([]int, p.End+1)
+	}
+	// 特殊处理第一行
+	var sum int
+	for i := 0; i <= p.End; i++ {
+		sum += p.Data[0][i]
+		statArr[0][i] = sum
+	}
+	//特殊处理第一列
+	sum = 0
+	for i := 0; i <= p.End; i++ {
+		sum += p.Data[i][0]
+		statArr[i][0] = sum
+	}
+	for i := 1; i <= p.End; i++ {
+		for j := 1; j <= p.End; j++ {
+			if statArr[i-1][j] < statArr[i][j-1] {
+				statArr[i][j] = statArr[i-1][j] + p.Data[i][j]
+			} else {
+				statArr[i][j] = statArr[i][j-1] + p.Data[i][j]
+			}
+		}
+	}
+	fmt.Println(statArr)
+	return statArr[p.End][p.End]
+}
+
 func main() {
 	data := make([][]int, 4)
 	data[0] = []int{1, 3, 5, 9}
@@ -58,4 +90,5 @@ func main() {
 	matrix := InitMatrix(data, 0, 0, 3)
 	matrix.Find(0, 0, 0)
 	fmt.Println(MinDist, count)
+	fmt.Println(matrix.Dyna())
 }

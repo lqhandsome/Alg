87e1bbf987b858f834da6f78ed32828fb7d7861c
lqhandsome <lqhandsome@163.com>
2023-05-09 22:13:51 +0800

    lianxi stack add method:add,pop

--- lianxi/one/2stackqueue/stack.go
+++ lianxi/one/2stackqueue/stack.go
@@ -5,7 +5,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// ArrStack 循环栈
+// ArrStack 循环栈 栈尾不存储数据
 type ArrStack struct {
 	data []string
 	head uint // 栈头
@@ -32,7 +32,7 @@ func InitArrStack(cap uint) *ArrStack {
 	}
 
 	return &ArrStack{
-		data: make([]string, cap),
+		data: []string{"null", "null", "null", "null", "null"},
 		head: 0,
 		tail: 0,
 		cap:  cap,
@@ -48,14 +48,15 @@ func (p *ArrStack) Range() {
 
 	if p.head == p.tail {
 		fmt.Println("stack is empty")
+		return
 	}
 
-	tmpHead := p.head
-	for tmpHead != p.tail {
-		fmt.Print(p.data[tmpHead])
-		tmpHead++
-		if tmpHead > p.cap {
-			tmpHead = 0
+	tmpTail := p.tail
+	for tmpTail != p.head {
+		fmt.Print(p.data[tmpTail], "-->")
+		tmpTail++
+		if tmpTail > p.cap-1 {
+			tmpTail = 0
 		}
 	}
 	fmt.Println()
@@ -67,7 +68,36 @@ func (p *ArrStack) Add(data string) error {
 		return fmt.Errorf("%s", " arrStack is nil")
 	}
 
+	if p.IsFull() {
+		log.Errorf("%s", "stack is full")
+
+		return fmt.Errorf("%s", "stack is full")
+	}
+
+	p.data[p.head] = data
+	p.head = (p.head + 1) % (p.cap)
 	return nil
+}
+
+// Pop 弹出一个元素
+func (p *ArrStack) Pop() (data string, e error) {
+
+	if p == nil || p.data == nil {
+		log.Errorf("%s", "p or data is nil")
+
+		return "null", fmt.Errorf("%s", "p or data is nil")
+	}
+
+	if p.IsEmpty() {
+
+		log.Errorf("%s", "stack is empty")
+
+		return "null", fmt.Errorf("%s", "stack is empty")
+	}
+
+	data = p.data[p.tail]
+	p.tail = (p.tail + 1) % p.cap
+	return
 }
 
 // IsEmpty 栈是否为空
@@ -89,8 +119,8 @@ func (p *ArrStack) IsFull() bool {
 		return true
 	}
 
-	if (p.tail+1+p.cap)%p.cap == p.head {
-
+	if (p.head+1)%(p.cap) == p.tail {
+		fmt.Printf("head: %v,tail: %v,cap: %v\r\n", p.head, p.tail, p.cap)
 		return true
 	} else {
 
--- lianxi/one/2stackqueue/stack_test.go
+++ lianxi/one/2stackqueue/stack_test.go
@@ -1,7 +1,53 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func Test_XXX(t *testing.T) {
+var (
+	arrStack  *ArrStack
+	listStack *ListStack
+)
+
+func TestMain(m *testing.M) {
+	arrStack = InitArrStack(5)
+	m.Run()
+}
+
+func Test_ArrStack_Add(t *testing.T) {
+
+	e := arrStack.Add("a")
+	if e != nil {
+		panic(e)
+	}
+	arrStack.Add("b")
+	arrStack.Add("c")
+	arrStack.Add("d")
+	arrStack.Add("e")
+	arrStack.Add("f")
+	fmt.Printf("%+v\r\n", arrStack)
+	arrStack.Range()
+}
+
+func Test_ArrStack_Pop(t *testing.T) {
+	arrStack.Add("b")
+	arrStack.Add("c")
+	arrStack.Add("d")
+	arrStack.Add("e")
+	arrStack.Add("f")
+
+	var data string
+	var e error
+	for !arrStack.IsEmpty() {
+		data, e = arrStack.Pop()
+		if e != nil {
+			panic(e)
+		}
+		fmt.Println(data)
+	}
+
+}
+func Test_ArrStack_Range(t *testing.T) {
 
 }

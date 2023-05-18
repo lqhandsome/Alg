a2cf894ded2df554e713682ec31270684a6736d3
lqhandsome <lqhandsome@163.com>
2023-05-18 22:05:21 +0800

    linxi:hashstring:add Readme

--- lianxi/one/5hashstring/hash.go
+++ lianxi/one/5hashstring/hash.go
@@ -1,1 +1,42 @@
-package _hashstring
+package main
+
+import (
+	list "container/list"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+)
+
+type hashList struct {
+	cap  uint
+	len  uint
+	data []hashList
+}
+
+func initHastList() *hashList {
+
+	return &hashList{}
+}
+func (h *hashList) Set(key string, value interface{}) error {
+
+	return nil
+}
+
+func (h *hashList) Get(key string) (value interface{}, e error) {
+
+	return
+}
+
+func main() {
+	l := list.New()
+	l.PushBack("a")
+	l.PushBack("b")
+	l.PushBack("c")
+	log.Info(l.Len())
+	log.Info(l.PushFront("d"))
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+
+}
+
+func hashFunc(str string) {}
